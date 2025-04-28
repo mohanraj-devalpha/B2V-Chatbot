87a2059a18670c58742a85cfc8f71b3d195a7a93
MOHANRAJ <mohanraj.s1039@ptuniv.edu.in>
2025-04-28 17:12:01 +0530

    emoji add for all the reponse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,6 +35,8 @@ const Chatbot: React.FC = () => {
         role: "system",
         content: `Introduce yourself as B2Vbot when the user greets.
          You are a helpful assistant for the B2V course providing website.
+         add the interactive emoji for all the message.
+
          List the courses as Listed Formatted with the  number not with other icons.
          You first greet the user, but do not mention anything about course details in your greeting.
          Greet only don't provide about the course details at the first greet.
